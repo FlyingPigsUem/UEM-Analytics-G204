c8d9b90b83ff403a581fa09ae1886cf03041254f
Sergio <gigighisler@gmail.com>
2020-11-29 15:30:01 +0100

    mod functions

--- firecast/functions/src/index.ts
+++ firecast/functions/src/index.ts
@@ -1,13 +1,16 @@
 import * as functions from 'firebase-functions';
 
- // Start writing Firebase Functions
- // https://firebase.google.com/docs/functions/typescript
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
- export const newValueFunction = functions.firestore.document('usuarios/{userId}').onCreate((snap, context) => {
+export const newValueFunction = functions.firestore.document('usuarios/{userId}').onCreate((snap, context) => {
     const newValue = snap.data();
-    
-    if(newValue){
-        return snap.ref.update({presion :10});
+
+    if (newValue) {
+        return snap.ref.update(
+        {presion: 10, 
+        hola: 'hola',
+         });
     }
     return null;
- });
+});

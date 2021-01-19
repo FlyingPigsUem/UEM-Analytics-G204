9060f237db58943d4fa7caf02280ef88baa860c9
Sergio <gigighisler@gmail.com>
2021-01-19 22:38:59 +0100

    Final version all working

--- mobileApp/smart_hospital/lib/values/customColors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-/// Contains a series of custom colors used on the App.
-class CustomColors {
-  static const mainBlue = Color(0xFF0367CC);
-  static const shadowBlue= Color(0xFF339DFA);
-  static const goodGreen= Color(0xFF1AC02B);
-}
